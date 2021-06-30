77830636b8cdffe33805501f655dc726e817f94a
adriel <adriel_sanmillan@yahoo.com>
2021-06-30 15:27:39 +0800

    v1.0

--- script.js
+++ script.js
@@ -3,31 +3,37 @@ const searchBox = document.querySelector('.city-search')
 const searchButton = document.querySelector('.search')
 const key = '21b57f0a705c4ca9b9a8f95335aabec3'
 
-searchButton.addEventListener('click', () => {
+searchButton.addEventListener('click', (event) => {
+  event.preventDefault();
   if (searchBox.value !== '') {
-    getData(searchBox.value)
+    const proxy = 'https://cors-anywhere.herokuapp.com/'
+    const url = `${proxy}https://api.weatherbit.io/v2.0/current?&city=${searchBox.value}&key=${key}`
+    fetch(url)
+      .then(response => response.json())
+      .then(data => setWeatherData(data))
   }
 })
-
-const getData = (city) => {
-  console.log('getData')
-  console.log(city.value)
-  fetch(`https://api.weatherbit.io/v2.0/current?&city=${city}&key=${key}&include=minutely`)
-    .then(data => data.json())
-    .then(setWeatherData)
-}
 
 const locationElement = document.querySelector('[data-location]')
 const statusElement = document.querySelector('[data-status]')
 const temperatureElement = document.querySelector('[data-temperature]')
-const precipitationElement = document.querySelector('[data-precipitation]')
+const humidityElement = document.querySelector('[data-humidity]')
 const windElement = document.querySelector('[data-wind]')
 
 const setWeatherData = (data) => {
-  console.log(data)
+  console.log(data.data[0])
+  console.log(data.data[0].weather.description)
 
-  temperatureElement.textContent = data.temp
-  precipitationElement.textContent = `${data.precip * 100}%`
-  windElement.textContent = data.windSpeed
-  weatherIcon.src = `https://www.weatherbit.io/static/img/icons/${data.weather.code}.png`
+  const weatherDesc = data.data[0].weather.description
+  const weatherDescCapitalized  = weatherDesc.split(' ').map((word) => { 
+        return word[0].toUpperCase() + word.substring(1); 
+    }).join(' ')
+
+  statusElement.textContent = weatherDescCapitalized
+  locationElement.textContent = data.data[0].city_name
+  temperatureElement.textContent = `${data.data[0].temp}°C`
+  humidityElement.textContent = `${+(data.data[0].rh).toFixed(1)}%`
+  windElement.textContent = `${+(data.data[0].wind_spd * 3.6).toFixed(1)} km/h`
+  weatherIcon.src = `https://www.weatherbit.io/static/img/icons/${data.data[0].weather.icon}.png`
+  document.body.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?${data.data[0].city_name}')`
 }
